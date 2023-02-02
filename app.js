bd93ebdd3f227484f6d5364d3bad6c9c8c90be10
david keukeu <davidkeukeu8@gmail.com>
2023-02-02 22:36:45 +0100

    hash password and generate token functions

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 require('dotenv').config()
 
+const { connectDB } = require('./src/services/mongoDB')
+
 const PORT =  process.env.PORT
 
 const app = express()
+connectDB()
 
 
 app.listen(PORT, () =>  console.log(`Server runing on port ${PORT}`))
---
+++ src/lib/password.js
@@ -0,0 +1,9 @@
+const bcrypt = require('bcrypt')
+
+const hashePassword = async (password) => {
+    const salt = await bcrypt.genSalt()
+    const passwordHash = await bcrypt.hash(password, salt)
+    return passwordHash
+}
+
+module.exports = hsdq
